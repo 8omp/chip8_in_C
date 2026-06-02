b20359846ea8cc1b2b30b8cf95637428bc3c1e3b
Bobompasato <mendechongtai@gmail.com>
2026-06-02 11:49:24 +0900

    feature: Define core CHIP-8 CPU structure

--- chip8.h
+++ chip8.h
@@ -3,42 +3,19 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+typedef struct  __chip8{
 
-#define MEMORY_SIZE 4096
-#define REGISTER_COUNT 16
-#define STACK_SIZE 16
-#define DISPLAY_WIDTH 64
-#define DISPLAY_HEIGHT 32
+    uint8_t memory[4096];
+    uint16_t I; //index register
+    uint8_t general_register[16];//V0 ~ VF, VFはフラグレジスタ
+    uint16_t pc;
 
-typedef struct{
-
-    uint8_t memory[MEMORY_SIZE]; 
-
-    /// 汎用レジスタ V0〜VF
-    uint8_t V[REGISTER_COUNT];
-
-    // インデックスレジスタ
-    uint16_t I;
-
-    // プログラムカウンタ
-    uint16_t PC;
-
-    // スタックポインタ
-    uint8_t SP;
-
-    uint16_t stack[STACK_SIZE];
+    uint8_t sp; //stack pointer
+    uint16_t stack[16];
 
     uint8_t delay_timer;
     uint8_t sound_timer;
 
-    uint8_t display[DISPLAY_WIDTH * DISPLAY_HEIGHT];
-
-    bool keypad[16];
-
 }CHIP8;
-
-void chip8_init(CHIP8 *chip8);
-void chip8_load_rom(CHIP8 *chip8, const char *filename);
-void chip8_reset(CHIP8 *chip8);
 
 #endif // CHIP8_H

0f9a32503423c458588b5c3dbbe168d60d870856
Bobompasato <mendechongtai@gmail.com>
2026-08-15 16:50:33 +0900

    fix: init_chip8(cpu) to init chip8(&cpu)

--- src/main.c
+++ src/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include "../include/chip8.h"
 
-int main(int argc, char *[]){
-    struct __chip8 *cpu;
-    init_chip8(cpu);
+int main(int argc, char *argv[]){
+    struct __chip8 cpu;
+    init_chip8(&cpu);
 
     char *filename = "../roms/IBM_Logo.ch8";
 
-    if(load_rom(cpu, filename)){
+    if(load_rom(&cpu, filename)){
         printf("Load success!\n");
-
-        for(int i=0; i<10; i++){
-            printf("%02X\n", cpu->memory[0x200 + i]);
+        while(1){
+            emulate_cycle(&cpu);
         }
+        
+    }else{
+        printf("Load failed\n");
+        return 1;
     }
 
     return 0;

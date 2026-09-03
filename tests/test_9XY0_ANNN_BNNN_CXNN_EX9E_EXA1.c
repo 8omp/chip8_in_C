2e3801a9dbcfd0d7f7eb455e23904a3658ee782e
Bobompasato <mendechongtai@gmail.com>
2026-09-03 12:06:42 +0900

    test: add 9XY0, ANNN, BNNN, CXNN, EX9E, EXA1 opcode test

--- tests/test_9XY0_ANNN_BNNN_CXNN_EX9E_EXA1.c
+++ tests/test_9XY0_ANNN_BNNN_CXNN_EX9E_EXA1.c
@@ -1,6 +1,6 @@
-//600F 610A 8015 61FF 8015 8016 8016 8017 6100 8017 6080 801E 801E 121Aでテスト
-
+//6001 6101 9010 ABCD B20B 60EE 60FF C001 E09E 60FF E0A1 1216でテスト
 #include <stdio.h>
+#include <time.h>
 #include "../include/chip8.h"
 
 int main(void){
@@ -9,14 +9,16 @@ int main(void){
     struct __chip8 cpu;
     init_chip8(&cpu);
 
-    char *filename = "./test_roms/8XY5_8XY6_8XY7_8XYE.ch8";
+    cpu.keypad = 0x0002;
+
+    char *filename = "./test_roms/9XY0_ANNN_BNNN_CXNN_EX9E_EXA1.ch8";
 
     if(load_rom(&cpu, filename)){
         printf("Load success!\n");
         while(1){
             emulate_cycle(&cpu);
 
-            printf("V[0]: 0x%02x, V[1]: 0x%02x, V[0xF]: 0x%02x, PC: 0x%03x", cpu.V[0], cpu.V[1], cpu.V[0xF], cpu.pc);
+            printf("V[0]: 0x%02x, V[1]: 0x%02x, keypad: 0x%04x, PC: 0x%03x, I: 0x%03x", cpu.V[0], cpu.V[1], cpu.keypad, cpu.pc, cpu.I);
             getchar(); //enterキー押すまで待機
         }
         

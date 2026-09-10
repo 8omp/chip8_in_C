f2bfad8c480c64f84b3a241abba97c88767719ea
Bobompasato <mendechongtai@gmail.com>
2026-09-10 15:18:53 +0900

    test: add DXYN opcode test

---
+++ tests/test_DXYN.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "../include/chip8.h"
+
+void draw_screen(CHIP8 cpu){
+    
+    for(int i = 0; i < 32; i++){
+        for(int j = 0; j < 64; j++){
+            if(cpu.display[i] & (1ULL << (63 - j))){
+                printf("■");
+            }else{
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+}
+
+int main(void){
+
+    srand((unsigned int)time(NULL));
+    struct __chip8 cpu;
+    init_chip8(&cpu);
+
+    char *filename = "../roms/IBM_Logo.ch8";
+
+    if(load_rom(&cpu, filename)){
+        printf("Load success!\n");
+        while(1){
+            emulate_cycle(&cpu);
+
+            draw_screen(cpu);
+            getchar(); //enterキー押すまで待機
+        }
+        
+    }else{
+        printf("Load failed\n");
+        return 1;
+    }
+
+    return 0;
+}

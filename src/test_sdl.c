dc68cbf9a3ef14baeeeed44946c9f414dbe7bd91
Bobompasato <mendechongtai@gmail.com>
2026-09-11 16:00:20 +0900

    SDL_Learn: render drawing and creating texture

--- src/test_sdl.c
+++ src/test_sdl.c
@@ -7,39 +7,58 @@ const int WINDOW_HEIGHT = 512;
 
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
-
-bool is_running = true;
+static SDL_Texture* texture = NULL;
 
 int main(int argc, char* argv[]){
 
     if(SDL_Init(SDL_INIT_VIDEO) != 0){
-        printf("SDLの初期化に失敗。error: %s\n", SDL_GetError());
+        printf("SDL Init failed. error: %s\n", SDL_GetError());
         return -1;
     }
 
     window = SDL_CreateWindow(u8"SDL2テスト", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     if(window == NULL){
-        printf("ウィンドウの初期化に失敗。error: %s", SDL_GetError());
+        printf("window init failed. error: %s", SDL_GetError());
         return -1;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if(renderer == NULL){
-        printf("ウィンドウの初期化に失敗。error: %s", SDL_GetError());
+        printf("renderer init failed. error: %s", SDL_GetError());
         return -1;
     }
 
-    while(is_running){
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 64, 32);
+    if(texture == NULL){
+        printf("renderer init failed. error: %s", SDL_GetError());
+        return -1;
+    }
+
+    bool isrunning = true;
+
+    while(isrunning){
         SDL_Event event;
+
         while(SDL_PollEvent(&event)){
             if(event.type == SDL_QUIT){
-                is_running = false;
+                isrunning = false;
             }
         }
+
+        //これで背景を決定してclear
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        //これで描く
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderDrawLine(renderer, 0, 0, 512, 256);
+
+        SDL_RenderPresent(renderer);
     }
 
-    SDL_DestroyWindow(window);
+    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;

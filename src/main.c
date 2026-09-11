7f76c958833918c7809843631948b24f88765cf2
Bobompasato <mendechongtai@gmail.com>
2026-09-11 16:20:29 +0900

    feat: add quit event feat

--- src/main.c
+++ src/main.c
@@ -53,6 +53,7 @@ int main(int argc, char *argv[])
             SDL_Event event;
             while (SDL_PollEvent(&event))
             {
+                
                 switch(event.type){
                     case SDL_QUIT:
                         isrunning = false;
